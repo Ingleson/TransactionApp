5e5466a304ec857fbd77759f6fcf6d6b9f30cc8d
Ingleson <jovemtksstranger@gmail.com>
2023-01-19 11:13:59 -0400

    toasty validation

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/index.sass';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ToastContainer } from 'react-toastify';
+
+import './styles/index.sass';
+import 'react-toastify/dist/ReactToastify.min.css'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ToastContainer 
+        position="top-right"
+        autoClose={1000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+      />
       <App />
     </QueryClientProvider>
   </React.StrictMode>,
